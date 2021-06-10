17d233f9bab1b88a2623db6b9e3ff98d2d20e980
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-06-10 15:30:41 -0500

    comment out favico in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html", 
     "/index.js",
     "/db.js" ,
-    "/favicon.ico", 
+    // "/favicon.ico", 
     "/styles.css", 
     "/icons/icon-192x192.png", 
     "/icons/icon-512x512.png"
