01dca27c778b7ce349ca7edb75a9df2d5cc62c22
cdfishe1 <75449671+cdfishe1@users.noreply.github.com>
2021-06-10 09:37:45 -0500

    create update, success, and error events for indexedDB

--- public/db.js
+++ public/db.js
@@ -4,3 +4,23 @@
 let db;
 const request = indexedDB.open("budget", 1);
 
+//create the onupgrade event with object store pending
+request.onupgradeneeded = function(event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+//create success event
+request.onsuccess = function(event) {
+    db = event.target.result;
+  
+    // checks if site is online before reading database
+    if (navigator.onLine) {
+      checkDatabase();
+    }
+};
+
+//create error event 
+request.onerror = function(event) {
+    console.log(event.target.errorCode);
+};
